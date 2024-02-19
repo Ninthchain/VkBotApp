c5e2f717e6acaaec68a6a19763692b6850d2c54c
Артём <ninthchainOFF@gmail.com>
2024-02-19 17:15:29 +0300

    Servlets are works!

--- src/main/java/com/dev/vkbot/HomeServlet.java
+++ src/main/java/com/dev/vkbot/HomeServlet.java
@@ -1,28 +1,18 @@
 package com.dev.vkbot;
 
-import jdk.javadoc.internal.doclets.toolkit.Content;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
-import java.io.PrintWriter;
 
-public class HomeServlet extends HttpServlet
-{
-
-	private static final long serialVersionUID = 1L;
-
-	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
-		{
-
-			res.setContentType(Content);
-
-			//We need printwriter object to write html content
-			PrintWriter pw = res.getWriter();
-
-			pw.close();// close the stream
-
+public class HomeServlet extends HttpServlet {
+		
+		@Override
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+				response.setContentType("application/json");
+				response.setStatus(HttpServletResponse.SC_OK);
 		}
 }
--- src/main/java/com/dev/vkbot/Main.java
+++ src/main/java/com/dev/vkbot/Main.java
@@ -1,15 +1,8 @@
 package com.dev.vkbot;
 
-import io.undertow.servlet.Servlets;
-import io.undertow.servlet.api.DeploymentInfo;
-import io.undertow.servlet.core.ErrorPages;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello fucking world!");
-
-        DeploymentInfo servletBuilder = Servlets.deployment();
-        servletBuilder.addWelcomePage("./index.jsp");
-
-    }
+		public static void main(String[] args) {
+		
+		}
 }
