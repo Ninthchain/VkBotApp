97d09117a34a78d84702d54b972f8487b2d9c62f
Артём <ninthchainOFF@gmail.com>
2024-02-27 21:19:41 +0300

    SellPointServlet literally works

--- src/main/java/com/dev/bot/controllers/PersonServlet.java
+++ src/main/java/com/dev/bot/controllers/PersonServlet.java
@@ -27,6 +27,7 @@ public class PersonServlet extends HttpServlet {
         personDao = new PersonDao();
     }
 
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("application/json");
@@ -41,32 +42,29 @@ public class PersonServlet extends HttpServlet {
         if (request.getParameter("id") != null) {
             person = personDao.getEntityById(Long.parseLong(request.getParameter("id")));
             if (person == null) return;
-            
+
             response.getWriter().print(new Gson().toJson(person));
             return;
         }
         if ((request.getParameter("vkId") != null)) {
             try {
-                
+
                 person = personDao.getEntitiesByColumnValue("vkId", Long.parseLong(request.getParameter("vkId"))).getFirst();
                 response.getWriter().print(new Gson().toJson(person));
                 response.getWriter().flush();
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 response.getWriter().print("null");
             }
         }
     }
-    
+
     /**
-     *
-     * @param request the {@link HttpServletRequest} object that contains the request the client made of the servlet
-     *
+     * @param request  the {@link HttpServletRequest} object that contains the request the client made of the servlet
      * @param response the {@link HttpServletResponse} object that contains the response the servlet returns to the client
-     * @apiNote  vkId must be not null
-     * @value status must be not null
      * @throws ServletException
      * @throws IOException
+     * @apiNote vkId must be not null
+     * @value status must be not null
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -76,16 +74,16 @@ public class PersonServlet extends HttpServlet {
             response.setStatus(400);
             return;
         }
-        
+
         Person person = new Person();
-        
+
         long vkId = Long.parseLong(request.getParameter("vkId"));
         person.setVkId(vkId);
         person.setStatus((short) 0);
 
-        if(request.getParameter("isVerified") != null)
+        if (request.getParameter("isVerified") != null)
             person.setIsVerified(Boolean.parseBoolean(request.getParameter("isVerified")));
-        if(request.getParameter("token") != null)
+        if (request.getParameter("token") != null)
             person.setToken(request.getParameter("token"));
         person.setIsVerified(false);
         this.personDao.persist(person);
@@ -94,50 +92,46 @@ public class PersonServlet extends HttpServlet {
 
         response.setStatus(200);
     }
-    
+
     /**
-     *
-     * @param request the {@link HttpServletRequest} object that contains the request the client made of the servlet
-     *
+     * @param request  the {@link HttpServletRequest} object that contains the request the client made of the servlet
      * @param response the {@link HttpServletResponse} object that contains the response the servlet returns to the client
-     * @value req param: vkId - not null
-     * @value req param: status - not null. 0 - unknown user. 1 - sending age, 2 - verifying, 3 - verified
      * @throws ServletException
      * @throws IOException
+     * @value req param: vkId - not null
+     * @value req param: status - not null. 0 - unknown user. 1 - sending age, 2 - verifying, 3 - verified
      */
     @Override
     protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        
+
         if (request.getParameter("vkId") == null) {
             response.setStatus(400);
             return;
         }
         long vkId = Long.parseLong(request.getParameter("vkId"));
-        short status = Short.parseShort(request.getParameter("status"));
-        
+
         Person person = new Person();
         person.setId(personDao.getEntitiesByColumnValue("vkId", vkId).getFirst().getId());
-        
+
         // must be not null
         person.setVkId(vkId);
-        
         // by default, it is false
         person.setIsVerified(false);
-        
-        if(request.getParameter("status") != null)
-            person.setStatus(status);
-        
-        if(request.getParameter("isVerified") != null)
+
+        if (request.getParameter("status") != null)
+            person.setStatus(Short.parseShort(request.getParameter("status")));
+
+        if (request.getParameter("isVerified") != null)
             person.setIsVerified(Boolean.parseBoolean(request.getParameter("isVerified")));
-        
-        if(request.getParameter("token") != null)
+
+        if (request.getParameter("token") != null)
             person.setToken(request.getParameter("token"));
-        
-        
+
+
         this.personDao.merge(person);
         response.setStatus(200);
     }
-    
+
 }
--- src/main/java/com/dev/bot/controllers/SellPointServlet.java
+++ src/main/java/com/dev/bot/controllers/SellPointServlet.java
@@ -13,113 +13,143 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "sell-point", urlPatterns = {"/db/sell-point"})
 public class SellPointServlet extends HttpServlet {
-		Dao<SellPoint> sellPointDao;
-		Gson gson;
-		@Override
-		public void init() throws ServletException {
-				super.init();
-				sellPointDao = new SellPointDao();
-				gson = new GsonBuilder().setPrettyPrinting().create();
-		}
-		
-		@Override
-		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-				resp.setContentType("application/json");
-				resp.setCharacterEncoding("UTF-8");
+    Dao<SellPoint> sellPointDao;
+    Gson gson;
 
-				if(req.getParameter("id") != null) {
-						SellPoint sellPoint = this.sellPointDao.getEntityById(Long.parseLong(req.getParameter("id")));
-						String jsonString = this.gson.toJson(sellPoint);
-						resp.getWriter().print(jsonString);
-						resp.getWriter().flush();
-						resp.setStatus(200);
-						return;
-				}
-				if(req.getParameter("all") != null) {
-						String jsonString = "";
-						if(this.isCityParameterValid(req)) {
-									jsonString = this.gson.toJson(this.sellPointDao.getEntitiesByColumnValue("city", req.getParameter("city")));
-						}
-						 else {
-								 jsonString = this.gson.toJson(this.sellPointDao.getAll());
-						 }
-						resp.getWriter().print(jsonString);
-						resp.getWriter().flush();
-						resp.setStatus(200);
-						return;
-				}
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        sellPointDao = new SellPointDao();
+        gson = new GsonBuilder().setPrettyPrinting().create();
+    }
 
-				if(!(this.isAddressParamValid(req) || this.isCityParameterValid(req))) {
-						resp.sendError(400, this.gson.toJson("{\"status_code\": 400, \"msg\": invalid request. City or Address param is null or empty}"));
-						return;
-				}
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
 
-				String address = req.getParameter("address");
-				SellPoint sellPoint = sellPointDao.getEntitiesByColumnValue("address", address).getFirst();
-				resp.getWriter().print(this.gson.toJson(sellPoint));
-				resp.getWriter().flush();
-				resp.setStatus(200);
-		}
+        if (req.getParameter("id") != null) {
+            SellPoint sellPoint = this.sellPointDao.getEntityById(Long.parseLong(req.getParameter("id")));
+            String jsonString = this.gson.toJson(sellPoint);
+            resp.getWriter().print(jsonString);
+            resp.getWriter().flush();
+            resp.setStatus(200);
+            return;
+        }
+        if (req.getParameter("all") != null) {
+            String jsonString = "";
+            if (this.isCityParameterValid(req)) {
+                jsonString = this.gson.toJson(this.sellPointDao.getEntitiesByColumnValue("city", req.getParameter("city")));
+            } else {
+                jsonString = this.gson.toJson(this.sellPointDao.getAll());
+            }
+            resp.getWriter().print(jsonString);
+            resp.getWriter().flush();
+            resp.setStatus(200);
+            return;
+        }
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		if(!(this.isAddressParamValid(req) || this.isCityParameterValid(req))) {
-			resp.sendError(400, this.gson.toJson("{\"status_code\": 400, \"msg\": invalid request. City or Address param is null or empty}"));
-		}
-		SellPoint sellPoint = new SellPoint();
-		sellPoint.setAddress(req.getParameter("address"));
-		sellPoint.setCity(req.getParameter("city"));
+        if (this.isAddressParamValid(req)) {
+            String address = req.getParameter("address");
+            try {
+                SellPoint sellPoint = sellPointDao.getEntitiesByColumnValue("address", address).getFirst();
+                resp.getWriter().print(this.gson.toJson(sellPoint));
+                resp.getWriter().flush();
+                resp.setStatus(200);
+            } catch (NullPointerException e) {
+                resp.getWriter().print("null");
+                resp.getWriter().flush();
+                resp.setStatus(400);
+            }
+            return;
+        } else if (this.isCityParameterValid(req)) {
+            String address = req.getParameter("city");
+            try {
+                SellPoint sellPoint = sellPointDao.getEntitiesByColumnValue("city", address).getFirst();
+                resp.getWriter().print(this.gson.toJson(sellPoint));
+                resp.getWriter().flush();
+                resp.setStatus(200);
+            } catch (NullPointerException e) {
+                resp.getWriter().print("null");
+                resp.getWriter().flush();
+                resp.setStatus(400);
+            }
 
-		if(req.getParameter("active") != null) {
-			boolean isActive = Boolean.parseBoolean(req.getParameter("active"));
-			sellPoint.setActive(isActive);
-		}
-		if(req.getParameter("sellerId") != null) {
-			long sellerId = Long.parseLong(req.getParameter("sellerId"));
-			sellPoint.setSellerId(sellerId);
-		}
-		this.sellPointDao.persist(sellPoint);
-		resp.getWriter().print(this.gson.toJson(sellPoint));
-		resp.getWriter().flush();
-		resp.setStatus(200);
-	}
+            return;
+        }
 
-	@Override
-	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		SellPoint sellPoint = null;
-		if(req.getParameter("id") != null) {
-			long id = Long.parseLong(req.getParameter("id"));
-			sellPoint = this.sellPointDao.getEntityById(id);
-		}
-		if(this.isAddressParamValid(req)) {
-			sellPoint = this.sellPointDao.getEntitiesByColumnValue("address", req.getParameter("address")).getFirst();
-		}
-		if(sellPoint == null) {
-			resp.sendError(400, "err");
-			return;
-		}
+        resp.sendError(400, this.gson.toJson("status_code: 400, msg: invalid request. City or Address param is null or empty"));
+    }
 
-		if(req.getParameter("active") != null) {
-			boolean isActive = Boolean.parseBoolean(req.getParameter("isActive"));
-			sellPoint.setActive(isActive);
-		}
-		if(req.getParameter("sellerId") != null) {
-			long sellerId = Long.parseLong(req.getParameter("sellerId"));
-			sellPoint.setSellerId(sellerId);
-		}
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (!(this.isAddressParamValid(req) || this.isCityParameterValid(req))) {
+            resp.sendError(400, this.gson.toJson("{\"status_code\": 400, \"msg\": invalid request. City or Address param is null or empty}"));
+        }
+        SellPoint sellPoint = new SellPoint();
+        sellPoint.setAddress(req.getParameter("address"));
+        sellPoint.setCity(req.getParameter("city"));
 
-		this.sellPointDao.merge(sellPoint);
-		resp.getWriter().print(this.gson.toJson(sellPoint));
-		resp.getWriter().flush();
-	}
+        if (req.getParameter("active") != null) {
+            boolean isActive = Boolean.parseBoolean(req.getParameter("active"));
+            sellPoint.setActive(isActive);
+        }
+        if (req.getParameter("sellerId") != null) {
+            long sellerId = Long.parseLong(req.getParameter("sellerId"));
+            sellPoint.setSellerId(sellerId);
+        }
+        this.sellPointDao.persist(sellPoint);
+        resp.getWriter().print(this.gson.toJson(sellPoint));
+        resp.getWriter().flush();
+        resp.setStatus(200);
+    }
 
-	private boolean isCityParameterValid(HttpServletRequest request) {
-			return (!request.getParameter("city").isEmpty() && request.getParameter("city") != null);
-	}
-	private boolean isAddressParamValid(HttpServletRequest request) {
-			return (!request.getParameter("address").isEmpty() && request.getParameter("city") != null);
-	}
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        SellPoint sellPoint = null;
+        if (req.getParameter("id") != null) {
+            long id = Long.parseLong(req.getParameter("id"));
+            sellPoint = this.sellPointDao.getEntityById(id);
+        }
+        if (this.isAddressParamValid(req)) {
+            sellPoint = this.sellPointDao.getEntitiesByColumnValue("address", req.getParameter("address")).getFirst();
+        }
+        if (sellPoint == null) {
+            resp.sendError(400, "err");
+            return;
+        }
+
+        if (req.getParameter("active") != null) {
+            boolean isActive = Boolean.parseBoolean(req.getParameter("isActive"));
+            sellPoint.setActive(isActive);
+        }
+        if (req.getParameter("sellerId") != null) {
+            long sellerId = Long.parseLong(req.getParameter("sellerId"));
+            sellPoint.setSellerId(sellerId);
+        }
+
+        this.sellPointDao.merge(sellPoint);
+        resp.getWriter().print(this.gson.toJson(sellPoint));
+        resp.getWriter().flush();
+    }
+
+    private boolean isCityParameterValid(HttpServletRequest request) {
+        if (request.getParameter("city") == null) {
+            return false;
+        }
+        System.out.println(!request.getParameter("city").isEmpty());
+        return !request.getParameter("city").isEmpty();
+    }
+
+    private boolean isAddressParamValid(HttpServletRequest request) {
+        if (request.getParameter("address") == null) {
+            return false;
+        }
+        boolean a = !request.getParameter("address").isEmpty();
+        return a;
+    }
 }
